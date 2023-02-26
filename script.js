fe3b557e56cf993d2c113ffbeefea8aba28026e3
gaurav8158 <120136122+gaurav8158@users.noreply.github.com>
2023-02-26 16:09:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your code here
-$(document).ready(function() {
-  var total = 0;
-  $('.price').each(function() {
-    total += parseFloat($(this).text());
-  });
-  $('.total').text(total.toFixed(2));
-});
+// $(document).ready(function() {
+//   var total = 0;
+//   $('.price').each(function() {
+//     total += parseFloat($(this).text());
+//   });
+//   $('.total').text(total.toFixed(2));
+// });
